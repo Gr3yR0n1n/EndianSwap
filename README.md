8f30a6b86522126788981abb33df57e0ce2152e3
Jean THOMAS <git0@pub.jeanthomas.me>
2017-11-04 16:11:08 +0100

    Almost rewrote everything : now support 16 bits swapping

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # EndianSwap
 
-Simple C utlity for changing the endianess of a file. Input file's length must be a multiple of 4 bytes.
+Simple C utlity for changing the endianess of a file. Input file's length must be a multiple of 2 or 4 bytes.
 
-## Howto
+## Compiling
 
 ```
 make
+```
+
+## Usage
+
+32 bits endianness swap :
+
+```
 bin/endianswap /my/input/file.bin /my/output/file.bin
+```
+
+16 bits endianness swap : 
+
+```
+bin/endianswap -n 16 input.bin output.bin
 ```
--- src/main.c
+++ src/main.c
@@ -23,53 +23,157 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <getopt.h>
+#include <errno.h>
+#include <ctype.h>
 
-#define bswap_16(value) \
-((((value) & 0xff) << 8) | ((value) >> 8))
+enum swap_type {
+  SWAP16,
+  SWAP32
+};
 
-#define bswap_32(value) \
-(((uint32_t)bswap_16((uint16_t)((value) & 0xffff)) << 16) | \
-(uint32_t)bswap_16((uint16_t)((value) >> 16)))
+uint16_t bswap16(uint16_t value) {
+  uint8_t byte1, byte2;
+
+  byte1 = value & 0xFF;
+  byte2 = (value >> 8) & 0xFF;
+
+  return byte1 << 8 | byte2;
+}
+
+uint32_t bswap32(uint32_t value) {
+  uint16_t word1, word2;
+
+  word1 = value & 0xFFFF;
+  word2 = (value >> 16) & 0xFFFF;
+
+  return bswap16(word1) << 16 | bswap16(word2);
+}
+
+int swapFile(char *input_file, char *output_file, enum swap_type swap_type) {
+  FILE *input_fh = NULL, *output_fh = NULL;
+  uint32_t current_dword;
+  uint16_t current_word;
+  int file_size, ret;
+
+  input_fh = fopen(input_file, "rb");
+  if (!input_fh) {
+    goto file_error;
+  }
+
+  output_fh = fopen(output_file, "wb");
+  if (!output_fh) {
+    goto file_error;
+  }
+
+  /* Get file length using portable libc code */
+  fseek(input_fh, 0, SEEK_END);
+  file_size = fseek(input_fh, 0, SEEK_END);
+  rewind(input_fh);
+
+  /* Check if the file length is correct */
+  if (swap_type == SWAP32 && file_size % 4 != 0) {
+    goto length_mult_error;
+  } else if (swap_type == SWAP16 && file_size % 2 != 0) {
+    goto length_mult_error;
+  }
+
+  while (!feof(input_fh)) {
+    if (swap_type == SWAP32) {
+      ret = fread(&current_dword, sizeof(uint32_t), 1, input_fh);
+      if (ret != 4) {
+	goto rw_error;
+      }
+
+      current_dword = bswap32(current_dword);
+      
+      ret = fwrite(&current_dword, sizeof(uint32_t), 1, output_fh);
+      if (ret != 4) {
+	goto rw_error;
+      }
+    } else if (swap_type == SWAP16) {
+      ret = fread(&current_word, sizeof(uint16_t), 1, input_fh);
+      if (ret != 2) {
+	goto rw_error;
+      }
+
+      current_word = bswap16(current_word);
+
+      ret = fwrite(&current_word, sizeof(uint16_t), 1, output_fh);
+      if (ret != 2) {
+	goto rw_error;
+      }
+    }
+  }
+
+  fclose(input_fh);
+  fclose(output_fh);
+
+  return 0;
+
+ file_error:
+  perror("Fatal error");
+  if (input_fh) {
+    fclose(input_fh);
+  }
+  if (output_fh) {
+    fclose(output_fh);
+  }
+  return -1;
+  
+ length_mult_error:
+  fprintf(stderr, "File length is not a multiple of 2 (16 bits endianness swap)"
+	  "or 4 (32 bits endianness swap). Please add NULL bytes using a"
+	  "hexadecimal editor.\n");
+  fclose(input_fh);
+  fclose(output_fh);
+  return -2;
+  
+ rw_error:
+  fprintf(stderr, "Read/write error\n");
+  fclose(input_fh);
+  fclose(output_fh);
+  return -3;
+}
 
 int main(int argc, char *argv[]) {
-  FILE *inputFile, *outputFile;
-  uint32_t currentDword;
-  
-  if (argc != 3) {
-    printf("USAGE: endianswap [input file] [output file]\n");
+  enum swap_type swap_type = SWAP32;
+  char c;
+
+  while ((c = getopt(argc, argv, "n:")) != -1) {
+    switch (c) {
+    case 'n':
+      if (strcmp(optarg, "32") == 0) {
+	swap_type = SWAP32;
+      } else if (strcmp(optarg, "16") == 0) {
+	swap_type = SWAP16;
+      } else {
+	fprintf(stderr, "Wrong swap type !\n");
+	return EXIT_FAILURE;
+      }
+      break;
+    case '?':
+      if (optopt == 'n') {
+	fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+      } else if (isprint(optopt)) {
+	fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+      } else {
+	fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+      }
+      return EXIT_FAILURE;
+      break;
+    default:
+      abort();
+    }
+  }
+
+  if (argc - optind != 2) {
+    printf("Usage: endianswap [-n 16/32] input.bin output.bin\n");
     return EXIT_SUCCESS;
   }
 
-  inputFile = fopen(argv[1], "rb");
-  if (inputFile == NULL) {
-    fprintf(stderr, "Error: Opening input file %s has failed.", argv[1]);
-    return EXIT_FAILURE;
-  }
+  swapFile(argv[optind], argv[optind+1], swap_type);
 
-  outputFile = fopen(argv[2], "wb");
-  if (outputFile == NULL) {
-    fprintf(stderr, "Error: Creating output file %s has failed.", argv[2]);
-    return EXIT_FAILURE;
-  }
-
-  while (!feof(inputFile)) {
-    fread(&currentDword, sizeof(uint32_t), 1, inputFile);
-    if (ferror(inputFile)) {
-      fprintf(stderr, "Error: Reading input file %s has failed.", argv[1]);
-      return EXIT_FAILURE;
-    }
-
-    currentDword = bswap_32(currentDword);
-    
-    fwrite(&currentDword, sizeof(uint32_t), 1, outputFile);
-    if (ferror(outputFile)) {
-      fprintf(stderr, "Error: Writing to output file %s has failed.", argv[2]);
-      return EXIT_FAILURE;
-    }
-  }
-
-  fclose(inputFile);
-  fclose(outputFile);
-  
   return EXIT_SUCCESS;
 }
